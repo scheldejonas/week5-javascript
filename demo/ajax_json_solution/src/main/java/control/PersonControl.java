9ff7be70eb453e8d1d9ca1065f3094a801b94072
Thomas Hartmann <tha@cphbusiness.dk>
2017-03-01 16:55:44 +0100

    ajax and json

--- demo/ajax_json_solution/src/main/java/control/PersonControl.java
+++ demo/ajax_json_solution/src/main/java/control/PersonControl.java
@@ -1,6 +1,9 @@
 package control;
 
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.Person;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -13,35 +16,25 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Thomas Hartmann - tha@cphbusiness.dk created on Mar 1, 2017
+ * @author Thomas Hartmann - tha@cphbusiness.dk created on Feb 26, 2017
  */
-@WebServlet(name="PersonControl", urlPatterns={"/PersonControl"})
+@WebServlet(urlPatterns={"/PersonControl"})
 public class PersonControl extends HttpServlet {
-    Gson gson = new Gson();
-    private static List<Person> persons = new ArrayList<Person>(){
-        {
-            add(new Person("Flemming", "Hansen", 23));
-            add(new Person("Hans", "Jensen", 54));
-            add(new Person("Connie", "Albertsen", 12));
-            add(new Person("Bente", "Petersen", 98));
-            add(new Person("Henrik", "Hansen", 32));
-        }
-    };
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            String jsonList = gson.toJson(persons, List.class);
-            out.print(jsonList);
-        }
-    } 
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//    Gson gson = new Gson();
+    private static int index = 0;
+   private static List<Person> persons = new ArrayList(){
+       {
+           add(new Person(1, "Hans", "Pedersen", 57));
+           add(new Person(2, "Gerda", "von Höve", 89));
+           add(new Person(3, "Frederik", "Brahms", 4));
+           add(new Person(4, "Hanne", "Jash", 46));
+           add(new Person(5, "Henriette", "Pust", 32));
+           add(new Person(6, "Aishe", "Hassimi", 51));
+           add(new Person(7, "Kasper", "Enoksen", 33));
+       }
+   };
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
@@ -54,7 +47,12 @@ public class PersonControl extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           String person = gson.toJson(persons.get(index));
+           if(++index >= persons.size()) index = 0;
+            out.print(person);
+        }
     } 
 
     /** 
@@ -67,8 +65,16 @@ public class PersonControl extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        
-        
+        try (PrintWriter out = response.getWriter()) {
+
+           request.getReader().lines().forEach((x)->{
+               Person p = gson.fromJson(x, Person.class);
+               p.setId(persons.size()+1);
+               persons.add(p);
+               out.println(p.getFirstname()+" "+p.getLastname()+" was added to the list");
+           });
+           //out.println(firstname);
+        }
     }
 
     /** 
